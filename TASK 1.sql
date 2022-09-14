e95e72531ec49763776f23fa96bde8bb90a91919
Sunidhi Thakur <38562071+sunidhi1404@users.noreply.github.com>
2022-09-14 15:09:35 +0530

    Update TASK 1.sql

--- TASK 1.sql
+++ TASK 1.sql
@@ -1,7 +1,10 @@
+/*
+Task 1: To create a visualization that provides a breakdown between the male and female employees working in the company each year, starting from 1990. 
+*/
+
 SELECT YEAR(de.from_date) AS calender_year, e.gender, COUNT(e.emp_no) AS no_of_employees
 FROM t_employees e
 JOIN t_dept_emp de ON de.emp_no = e.emp_no
 GROUP BY calender_year, e.gender
 HAVING calender_year >= 1990
 ORDER BY calender_year;
-
